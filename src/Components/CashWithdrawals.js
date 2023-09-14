2eb6e1ff8d6b584ce07176fc9b97826932d0e85b
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-09-14 11:22:30 +0100

    commit

--- src/Components/CashWithdrawals.js
+++ src/Components/CashWithdrawals.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import noteData from '../Data/NoteData'; // Import noteData from your file
 import '../StyleSheet/cash-withdraw.css';
 import MainLayout from '../MainLayout';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const ATMApp = () => {
-  const [balance, setBalance] = useState(220);
+  const initialBalance = 220;
   const overdraftAllowance = 100;
+  const [balance, setBalance] = useState(initialBalance);
   const [withdrawalAmount, setWithdrawalAmount] = useState(0);
   const [dispensedAmount, setDispensedAmount] = useState(0);
   const [notesDispensed, setNotesDispensed] = useState([]);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [updatedNotesData, setUpdatedNotesData] = useState(noteData);
 
   const handleWithdrawal = () => {
     // Calculate the remaining balance after withdrawal
@@ -20,10 +26,13 @@ const ATMApp = () => {
       let remainingAmountToDispense = withdrawalAmount;
       const dispensedNotes = [];
 
+      // Create a copy of the notes data to update
+      const updatedNotes = [...updatedNotesData];
+
       // Iterate through available notes and dispense them
-      for (const note of noteData) {
+      for (const [index, note] of updatedNotes.entries()) {
         const noteValue = note.value;
-        const availableNotes = note.count;
+        let availableNotes = note.count;
 
         const notesToDispense = Math.min(
           Math.floor(remainingAmountToDispense / noteValue),
@@ -33,6 +42,8 @@ const ATMApp = () => {
         if (notesToDispense > 0) {
           dispensedNotes.push({ noteValue, count: notesToDispense });
           remainingAmountToDispense -= notesToDispense * noteValue;
+          availableNotes -= notesToDispense;
+          updatedNotes[index] = { ...note, count: availableNotes };
         }
 
         if (remainingAmountToDispense === 0) break;
@@ -41,26 +52,45 @@ const ATMApp = () => {
       // Update state with dispensed amount and notes
       setDispensedAmount(withdrawalAmount - remainingAmountToDispense);
       setNotesDispensed(dispensedNotes);
+      setUpdatedNotesData(updatedNotes);
 
-      // Update balance
-      setBalance(remainingBalance);
+      // Show the withdrawal confirmation modal
+      setShowConfirmationModal(true);
     } else {
-      // User is going overdrawn, handle as needed
-      // You can show an error message or take other actions here
+      alert('Withdrawal amount exceeds overdraft allowance.');
     }
+  };
+
+  const handleConfirmationYes = () => {
+    // Update balance
+    const newBalance = balance - withdrawalAmount;
+    setBalance(newBalance);
+
+    // Reset withdrawal input
+    setWithdrawalAmount(0);
+
+    // Reset confirmation modal
+    setShowConfirmationModal(false);
+    alert('thank you')
+  };
+
+  const handleConfirmationNo = () => {
+    setShowConfirmationModal(false);
+    alert('Thank you for using the service.');
   };
 
   return (
     <MainLayout>
       <div className='cash-withdrawal-box'>
-        <h1>ATM App</h1>
-        <p>Current Balance: £{balance}</p>
-        <p>Overdraft Allowance: £{overdraftAllowance}</p>
-
+        <div className='balance'>
+          <h1>Get Cash Now</h1>
+          <p>YourCurrent Balance: £{balance}</p>
+          <p>Your Overdraft Allowance: £{overdraftAllowance}</p>
+        </div>
         <div>
           <h2>Available Notes</h2>
           <ul>
-            {noteData.map((note) => (
+            {updatedNotesData.map((note) => (
               <li key={note.value}>
                 £{note.value} notes: {note.count} available
               </li>
@@ -68,10 +98,10 @@ const ATMApp = () => {
           </ul>
         </div>
 
-        <div>
-          <h2>Withdrawal</h2>
+        <div className='withdraw-input'>
+          <h2>How much do you want to withdraw?</h2>
           <label>
-            Enter Withdrawal Amount: £
+            Enter Amount: £
             <input
               type="number"
               value={withdrawalAmount}
@@ -81,21 +111,17 @@ const ATMApp = () => {
           <button onClick={handleWithdrawal}>Submit</button>
         </div>
 
-        {dispensedAmount > 0 && (
-          <div>
-            <h2>Dispensed Amount</h2>
+        {showConfirmationModal && (
+          <div className="confirmation-modal">
             <p>Dispensed Amount: £{dispensedAmount}</p>
-            <p>Total Notes Dispensed:</p>
-            <ul>
-              {notesDispensed.map((note) => (
-                <li key={note.noteValue}>
-                  £{note.noteValue} notes: {note.count}
-                </li>
-              ))}
-            </ul>
+            <p>Total Notes Dispensed: {notesDispensed.length}</p>
+            <p>Do you want to process?</p>
+            <button onClick={handleConfirmationYes}>Yes</button>
+            <button onClick={handleConfirmationNo}>No</button>
           </div>
         )}
       </div>
+      
     </MainLayout>
   );
 };
