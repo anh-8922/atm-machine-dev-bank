c189d146a29c5dba2f3c0d793367fb93fbecc2d1
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-09-15 13:25:37 +0100

    test sucessfully

--- src/Testing/CashWithdrawals.test.js
+++ src/Testing/CashWithdrawals.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter, Route } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
-import ATMApp from "../Components/ATMApp"; // Adjust the import to your file structure
+import ATMApp from "../Components/CashWithdrawals"; // Adjust the import to your file structure
 
 // Mock noteData
 const mockNoteData = [
