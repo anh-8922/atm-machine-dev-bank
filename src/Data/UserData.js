0ff94ad4caf23b751607049c09a843bba36e1b21
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-09-17 10:54:26 +0100

    update userData

--- src/Data/UserData.js
+++ src/Data/UserData.js
@@ -1,5 +1,9 @@
 const userData = [
     { name: 'michael', balance: 220, overdraft: 100 },
+    { name: 'Masha', balance: 150, overdraft: 100 },
+    { name: 'Arpo', balance: 200, overdraft: 100 },
+    { name: 'Gabi', balance: 300, overdraft: 100 },
+    { name: 'Sesshomaru', balance: 270, overdraft: 100 },
   ];
   
   export default userData;
